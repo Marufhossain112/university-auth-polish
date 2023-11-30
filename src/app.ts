282e9cebd63de14b3256805f6f38113823ae53fc
Maruf Hossain <108384673+Marufhossain112@users.noreply.github.com>
2023-11-30 11:51:09 +0600

    install cors

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express, { Application, Request, Response } from "express";
+import cors from "cors";
 const app: Application = express();
-const port = 5000;
-
+// use cors
+app.use(cors());
+// parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
